0edea7de60458879ac2723c6d54aaf6d3a274778
Sounarva Bardhan <sounarvabardhan2003@gmail.com>
2026-03-04 22:11:16 +0530

    Deployment bug fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,6 +6,7 @@ const authRoutes = require('./routes/auth.routes')
 const songRoutes = require('./routes/song.routes')
 const path = require("path")
 app.use(express.json())
+app.use(express.static('./public'))
 app.use(cookieParser())
 
 app.use("/api/v1/auth", authRoutes)
